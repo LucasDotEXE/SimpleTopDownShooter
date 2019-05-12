e3eb9013fa98183f3351864bb21e4dd4b39c805f
Lucas Prins <lljPrins@avans.nl>
2019-05-12 15:29:59 +0200

    Added the server asking for a pin

--- SimpleShooter/src/avans/shooter/Server/ServerMain.java
+++ SimpleShooter/src/avans/shooter/Server/ServerMain.java
@@ -1,7 +1,26 @@
 package avans.shooter.Server;
 
+import java.util.Scanner;
+
 public class ServerMain {
     public static void main(String[] args) {
+        int portPin = askPin();
+        System.out.println("Selecting a map...");
 
+
+    }
+
+    private static int askPin() {
+        System.out.println("Enter port pin:");
+        System.out.print("> ");
+        int pin;
+        Scanner reader = new Scanner(System.in);
+        try {
+            pin = Integer.parseInt(reader.next());
+        } catch (NumberFormatException e) {
+            System.out.println("That was not a valid port pin, pleas enter a number. (preferably 4 digits or higher)");
+            pin = askPin();
+        }
+        return pin;
     }
 }
