75e557b981bbf3794b3f2ca72dc9600ed2acbf67
Lucas Prins <lljPrins@avans.nl>
2019-05-13 13:12:41 +0200

    Added a non functioning login screen

--- SimpleShooter/src/avans/shooter/Client/ClientMain.java
+++ SimpleShooter/src/avans/shooter/Client/ClientMain.java
@@ -1,7 +1,41 @@
 package avans.shooter.Client;
 
-public class ClientMain {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
+
+
+public class ClientMain extends Application {
+    public static void main(String[] args) {
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        openLogin(primaryStage);
+
+        primaryStage.show();
+    }
+
+    private void openLogin(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+
+            primaryStage.setScene(new Scene(root, 175, 155));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
